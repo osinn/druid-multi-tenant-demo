5cc02d6bd193b7d774fecd771de766dbe11f9c66
wency_cai <javawency@163.com>
2022-06-30 21:05:56 +0800

    多租户配置

--- src/test/java/com/example/mybatisplus/tenant/TenantDemo.java
+++ src/test/java/com/example/mybatisplus/tenant/TenantDemo.java
@@ -1,45 +1,45 @@
-package com.example.mybatisplus.tenant;
-
-import com.github.osinn.druid.multi.tenant.plugin.handler.TenantInfoHandler;
-import com.github.osinn.druid.multi.tenant.plugin.parser.DefaultSqlParser;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * 描述
- *
- * @author wency_cai
- */
-public class TenantDemo {
-
-    private static final String sql1 = "SELECT id, name,tenant_id FROM role";
-
-    public static void main(String[] args) {
-        DefaultSqlParser defaultSqlParser = new DefaultSqlParser();
-        defaultSqlParser.setTenantInfoHandler(new TenantInfoHandler() {
-            @Override
-            public List getTenantIds() {
-                Long tenantId = 1540616714700591104L;
-                Long tenantId2 = 1540616731523944448L;
-                List<Long> tenantIdList = new ArrayList<>();
-                tenantIdList.add(tenantId);
-                tenantIdList.add(tenantId2);
-                return tenantIdList;
-            }
-
-            @Override
-            public List<String> ignoreTablePrefix() {
-                return new ArrayList<>();
-            }
-
-            @Override
-            public String getTenantIdColumn() {
-                return "tenant_id";
-            }
-        });
-
-        String newSQL = defaultSqlParser.setTenantParameter(sql1);
-        System.out.println(newSQL);
-    }
-}
+//package com.example.mybatisplus.tenant;
+//
+//import com.github.osinn.druid.multi.tenant.plugin.handler.TenantInfoHandler;
+//import com.github.osinn.druid.multi.tenant.plugin.parser.DefaultSqlParser;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * 描述
+// *
+// * @author wency_cai
+// */
+//public class TenantDemo {
+//
+//    private static final String sql1 = "SELECT id, name,tenant_id FROM role";
+//
+//    public static void main(String[] args) {
+//        DefaultSqlParser defaultSqlParser = new DefaultSqlParser();
+//        defaultSqlParser.setTenantInfoHandler(new TenantInfoHandler() {
+//            @Override
+//            public List getTenantIds() {
+//                Long tenantId = 1540616714700591104L;
+//                Long tenantId2 = 1540616731523944448L;
+//                List<Long> tenantIdList = new ArrayList<>();
+//                tenantIdList.add(tenantId);
+//                tenantIdList.add(tenantId2);
+//                return tenantIdList;
+//            }
+//
+//            @Override
+//            public List<String> ignoreTablePrefix() {
+//                return new ArrayList<>();
+//            }
+//
+//            @Override
+//            public String getTenantIdColumn() {
+//                return "tenant_id";
+//            }
+//        });
+//
+//        String newSQL = defaultSqlParser.setTenantParameter(sql1);
+//        System.out.println(newSQL);
+//    }
+//}
