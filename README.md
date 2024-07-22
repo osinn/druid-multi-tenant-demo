3be613f9acb25916478b3fab8d9fe164584331ac
wency_cai <>
2024-07-22 10:10:09 +0800

    Spring boot3 演示

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # druid-multi-tenant-demo
 
 #### 项目介绍
-- druid-multi-tenant-starter 多租户插件演示
+- druid-multi-tenant-starter 多租户插件Spring boot3 演示
--- src/main/java/com/github/osinn/example/multi/tenant/controller/DemoController.java
+++ src/main/java/com/github/osinn/example/multi/tenant/controller/DemoController.java
@@ -3,11 +3,12 @@ package com.github.osinn.example.multi.tenant.controller;
 import com.github.osinn.example.multi.tenant.entity.User;
 import com.github.osinn.example.multi.tenant.mapper.UserMapper;
 import com.github.osinn.example.multi.tenant.service.IUserService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -16,6 +17,7 @@ import java.util.List;
  * @author wency_cai
  */
 //@DS("salesAfter")
+    @Slf4j
 @RestController
 public class DemoController {
     @Autowired
@@ -42,6 +44,12 @@ public class DemoController {
         tenantId = 2L;
         User user2 = userMapper.getUserInfoSQLById(2L, tenantId);
 //        System.out.println(user2);
+
+        User user3 = new User();
+//        user3.setTenantId(tenantId);
+        user3.setAge(12);
+        user3.setName("测试1");
+        userMapper.insert(user3);
         return user;
     }
 }
